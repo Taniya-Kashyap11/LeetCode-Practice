6025fdfb092413face22c2e7cb79d2498707418c
Taniya-Kashyap11 <taniyakashyap2005@gmail.com>
2026-08-28 22:43:59 +0530

    Time: 0 ms (100%), Space: 8.9 MB (37.31%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -1,22 +1,24 @@
 class Solution {
 public:
     bool isValid(string s) {
-        stack<char> st;
-        for(int i=0;i<s.size();i++)
-        {
-            if(s[i]=='(' || s[i]=='[' || s[i]=='{'){
-                st.push(s[i]);
-            }else{
-                if(st.empty()) return false;
-                char ch=st.top();
+     stack<char> st;
+     if(s.size()<=1){
+        return false;
+     }
+     for(int i=0;i<s.size();i++){
+        if(s[i]=='(' || s[i]=='{' || s[i]=='['){
+            st.push(s[i]);
+        }
+        else{
+            if(st.empty()) return false;
+            char ch=st.top();
+            if((ch=='(' && s[i]==')') || (ch=='{' && s[i]=='}')|| (ch=='[' && s[i]==']')){
                 st.pop();
-                if((ch=='(' && s[i]==')') ||( ch=='['  && s[i]==']')|| (ch=='{' && s[i]=='}')){
-
-                }else{
-                    return false;
-                }
+            }else{
+                return false;
             }
         }
-        return st.empty();
+     }
+     return st.empty();
     }
 };

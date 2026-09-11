32267c42a04251814fecd29eb4b8a2854b0237a2
Taniya-Kashyap11 <taniyakashyap2005@gmail.com>
2026-09-11 22:02:37 +0530

    Time: 6 ms (21.9%), Space: 9.9 MB (11.39%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,15 +1,34 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        unordered_map<int,int> m1;
-        unordered_map<int,int> m2;
-        if(s.size()!=t.size())
-        return false;
-        for(int i=0;i<s.size();i++){
-            m1[s[i]]++;
-            m2[t[i]]++;
+        // unordered_map<int,int> m1;
+        // unordered_map<int,int> m2;
+        // if(s.size()!=t.size())
+        // return false;
+        // for(int i=0;i<s.size();i++){
+        //     m1[s[i]]++;
+        //     m2[t[i]]++;
+        // }
+        // if(m1==m2) return true;
+        // return false;
+        if(s.size() != t.size()){
+            return false;
         }
-        if(m1==m2) return true;
-        return false;
+        unordered_map<int,int>mp;
+        for(int i=0;i<s.size();i++){
+            if(!mp[s[i]]){
+                mp[s[i]]=1;
+            }else{
+                mp[s[i]]++;
+            }
+        }
+        for(int i=0;i<t.size();i++){
+            if(!mp[t[i]] || mp[t[i]]<0){
+                return false;
+            }else{
+                mp[t[i]]--;
+            }
+        }
+        return true;
     }
 };

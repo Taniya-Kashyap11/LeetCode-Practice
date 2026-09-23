41e1e73d685aa801873fa86b27ae06c8a4c83d74
Taniya-Kashyap11 <taniyakashyap2005@gmail.com>
2026-09-23 20:48:33 +0530

    Time: 1 ms (42.71%), Space: 9.9 MB (76.77%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int i=0;
+        int j=s.size()-1;
+        while(i<j){
+            if(!isalnum(s[i])){
+                i++;
+            }else if(!isalnum(s[j])){
+                j--;
+            }
+            else if(s[i]==' '){
+                i++;
+            }else if(s[j]==' '){
+                j--;
+            }else if(tolower(s[i])==tolower(s[j])){
+                i++;
+                j--;
+            }else{
+                return false;
+            }
+        }
+        return true;
+    }
+};

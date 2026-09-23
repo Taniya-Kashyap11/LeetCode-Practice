dff2c46e30ea5fdc743085fbc2d9f84acf493844
Taniya-Kashyap11 <taniyakashyap2005@gmail.com>
2026-09-23 21:41:15 +0530

    Time: 3 ms (33.93%), Space: 11.2 MB (23.41%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -1,33 +1,31 @@
 class Solution {
 public:
     string reverseWords(string s) {
-        vector<string>words;
-        string word;
-        string result;
-        for(int i=0;i<s.size();i++)
+     vector<string> words;
+     string result;
+     string word;
+     for(int i=0;i<s.size();i++){
+        if(s[i]!=' ')
         {
-            if(s[i]!=' ')
-            {
-                   word+=s[i];
-            }else if(!word.empty())
-            {
-                words.push_back(word);
-                word="";
-            }
-       
-        }
-        if(word.size()>0)
-        {
+            word+=s[i];
+        }else if(!word.empty()){
             words.push_back(word);
+            word="";
         }
-        reverse(words.begin(),words.end());
-        for(int i=0;i<words.size();i++){
-            result+=words[i];
-            if(i<words.size()-1)
-            {
-                result+=" ";
-            }
+    
+     }
+     if(word.size()>0)
+     {
+        words.push_back(word);
+     }
+     reverse(words.begin(),words.end());
+     for(int i=0;i<words.size();i++){
+        result+=words[i];
+        if(i<words.size()-1){
+              result+=' ';
         }
-        return result;
+      
+     }
+     return result;
     }
 };
